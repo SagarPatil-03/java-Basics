d68e5f057d7863ed9aef20e9242839f28148eeb1
Sagar Patil <sagardpatil03@gmail.com>
2026-07-07 10:06:38 +0530

    Initial commit

---
+++ Fact.java
@@ -0,0 +1,14 @@
+class Fact
+{
+    public static void main(String args[])
+    {
+        int fact = 1;
+
+        for(int num =12;num>=1;num--)
+        {
+      fact = fact*num;
+        }
+        System.out.println(fact);
+    }
+
+}
