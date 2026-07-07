54492c58c7fedc11f9ae1359bcd7bfd806b1b3f1
Sagar Patil <sagardpatil03@gmail.com>
2026-07-07 10:11:00 +0530

    Updated RevNum.java

--- RevNum.java
+++ RevNum.java
@@ -1,11 +1,11 @@
 import java.util.*;
+
 public class RevNum {
     public static void main(String[] args) {
-        int num=345;
+        int num = 345;
 
-        String rev= new StringBuffer(String.valueOf(num)).reverse().toString();
+        String rev = new StringBuffer(String.valueOf(num)).reverse().toString();
 
         System.out.println(rev);
     }
-
 }
