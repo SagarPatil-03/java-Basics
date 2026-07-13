4eca44db08b71753349296ac49ec48d1834d7c9e
Sagar Patil <sagardpatil03@gmail.com>
2026-07-13 18:23:28 +0530

    Added Armstrong Number program

---
+++ ArmStrongNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            sum = sum + (rem * rem * rem);
+            num = num / 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong Number");
+        } else {
+            System.out.println(original + " is Not an Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
