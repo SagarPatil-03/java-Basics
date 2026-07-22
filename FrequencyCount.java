0bcf5aa2eb1ee10b0c51cdb5b0a47fa585a48266
Sagar Patil <sagardpatil03@gmail.com>
2026-07-22 09:13:24 +0530

    sagar

---
+++ FrequencyCount.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+public class FrequencyCount {
+
+
+    
+
+public static void main(String args[])
+{
+        String str = "banana";
+
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
+
+    
+
