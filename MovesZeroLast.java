b8240313d14cc9344881d4181148ecda2b1c91d7
Sagar Patil <sagardpatil03@gmail.com>
2026-07-23 18:27:03 +0530

    up

---
+++ MovesZeroLast.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MovesZeroLast {
+    
+    public static void main(String args[])
+
+    {
+
+        int arr[]={1,0,9,0,7};
+        int j=0;
+
+         for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
