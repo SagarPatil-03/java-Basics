ae6556d913413cfd9fb858ab435f75b57c5c3627
Sagar Patil <sagardpatil03@gmail.com>
2026-07-26 11:49:18 +0530

    reverse linked list

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,53 +1,46 @@
-public class ReverseLinkedList {
+class Node {
+    int data;
+    Node next;
 
-    static class Node {
-        int data;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class ReverseLinkedList {
+
+    static Node rev(Node head) {
+
+        Node p = null;
+        Node curr = head;
         Node next;
 
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-
-        while (current != null) {
-            next = current.next;   // Store next node
-            current.next = prev;   // Reverse link
-            prev = current;        // Move prev forward
-            current = next;        // Move current forward
+        while (curr != null) {
+            next = curr.next;
+            curr.next = p;
+            p = curr;
+            curr = next;
         }
 
-        return prev;
+        return p;
     }
 
-    public static void printList(Node head) {
-        while (head != null) {
-            System.out.print(head.data + " -> ");
-            head = head.next;
+    public static void main(String args[]) {
+
+        Node f = new Node(10);
+        Node s = new Node(20);
+        Node t = new Node(30);
+
+        f.next = s;
+        s.next = t;
+
+        f = rev(f);
+
+        Node temp = f;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
         }
-        System.out.println("null");
-    }
-
-    public static void main(String[] args) {
-
-        // Create Linked List: 1 -> 2 -> 3 -> 4 -> 5
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
-
-        System.out.println("Original Linked List:");
-        printList(head);
-
-        head = reverse(head);
-
-        System.out.println("Reversed Linked List:");
-        printList(head);
     }
 }
