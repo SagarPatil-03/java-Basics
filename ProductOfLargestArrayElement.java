5f13cd9b2c9d8cee2f0ec7335e11fc9f89f1a16d
Sagar Patil <sagardpatil03@gmail.com>
2026-07-28 09:34:19 +0530

    m

---
+++ ProductOfLargestArrayElement.java
@@ -0,0 +1,31 @@
+public class ProductOfLargestArrayElement {
+
+    public static void main(String args[])
+    {
+        int arr1[]={1,2,3,4};
+        int arr2[]={5,6,7,8};
+
+        int max1=arr1[0];
+        int max2=arr2[0];
+
+        for(int num:arr1)
+        {
+            if(num>max1)
+            {
+                max1=num;
+            }
+        }
+
+        for(int num:arr2)
+        {
+            if(num>max2)
+            {
+                max2=num;
+            }
+        }
+
+        System.out.println(max1*max2);
+
+    }
+    
+}
