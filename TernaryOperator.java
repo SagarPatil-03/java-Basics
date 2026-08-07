a2630a160e887367253e0367313199acb25501fc
Sagar Patil <sagardpatil03@gmail.com>
2026-08-07 18:08:37 +0530

    Update project

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int num = 15;
+
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(result);
+    }
+}
+    
+
