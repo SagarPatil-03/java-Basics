27803b6f3f4442223f28553616a567c3ad7a8050
Sagar Patil <sagardpatil03@gmail.com>
2026-08-11 14:28:34 +0530

    Update project

---
+++ Singleton.java
@@ -0,0 +1,26 @@
+class Main {
+
+    private static Main obj;
+
+    private Main() {
+    }
+
+    public static Main getInstance() {
+
+        if (obj == null) {
+            obj = new Main();
+        }
+
+        return obj;
+    }
+}
+
+class Singleton {
+    public static void main(String[] args) {
+
+        Main s1 = Main.getInstance();
+        Main s2 = Main.getInstance();
+
+        System.out.println(s1 == s2);
+    }
+}
