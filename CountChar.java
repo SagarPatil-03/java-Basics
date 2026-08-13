9e5f897cdfbb9e286106d4dc31a7fc9a832e02dc
Sagar Patil <sagardpatil03@gmail.com>
2026-08-13 10:18:25 +0530

    Update project

---
+++ CountChar.java
@@ -0,0 +1,17 @@
+public class CountChar {
+    
+    public static void main(String args[])
+    {
+        String str="sagar";
+        int count=1;
+        String s="";
+
+        for(int i=0;i<str.length()-1;i++)
+        {
+         s=s+str.charAt(i);
+         count++;
+        }
+
+        System.out.println(count);
+    }
+}
