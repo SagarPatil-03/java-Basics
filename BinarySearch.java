9d56481ade87f26f78db655074b1fe9a81b09d11
Sagar Patil <sagardpatil03@gmail.com>
2026-08-13 09:59:01 +0530

    Update project

---
+++ BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static void main(String args[])
+    {
+   
+        int arr[]={1,2,3,4,5,6,7};
+        int key =3;
+
+        int start=0;
+        int end = arr.length-1;
+
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+
+            if(arr[mid]==key)
+            {
+                System.out.println(mid);
+            }
+        }
+
+
+
+    }
+    
+}
