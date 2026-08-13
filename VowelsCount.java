6693db66f9051133b55d33a0715d74b762af03e4
Sagar Patil <sagardpatil03@gmail.com>
2026-08-13 10:27:37 +0530

    Update project

---
+++ VowelsCount.java
@@ -0,0 +1,22 @@
+public class VowelsCount {
+
+    public static void main(String args[])
+    {
+     
+        String str ="sagar";
+        int count=0;
+
+        for(int i=0;i<str.length()-1;i++)
+
+        {
+            char ch = str.charAt(i);
+            if(ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u'  )
+            {
+                count++;
+            }
+        }
+
+
+    }
+    
+}
