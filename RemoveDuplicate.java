091af4d31f4ab2d304d59c266d1052ccc4db226a
Sagar Patil <sagardpatil03@gmail.com>
2026-08-18 09:50:37 +0530

    Update project

---
+++ RemoveDuplicate.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicate {
+
+    public static void main(String args[])
+    {
+        int arr[]={1,1,2,3,2,5};
+
+        int j=1;
+
+        for(int i=1;i<arr.length;i++)
+        {
+            if(arr[j-1]!=arr[i])
+            {
+              arr[j]=arr[i];
+              j++;
+
+            }
+        }
+    }
+    
+}
