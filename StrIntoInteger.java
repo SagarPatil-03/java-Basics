e8db908750d99c7ed42a72f956667e53f36c2269
Sagar Patil <sagardpatil03@gmail.com>
2026-08-19 10:09:13 +0530

    Update project

---
+++ StrIntoInteger.java
@@ -0,0 +1,10 @@
+class StrIntoInteger{
+    public static void main(String args[])
+    {
+        String s="223";
+
+        int num=Integer.parseInt(s);
+
+        System.out.println(num);
+    }
+}
