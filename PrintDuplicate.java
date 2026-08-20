e036bd25efaa448dd1775f957b499a9d186ae0ef
Sagar Patil <sagardpatil03@gmail.com>
2026-08-20 17:02:00 +0530

    Update project

---
+++ PrintDuplicate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class PrintDuplicate{
+    public static void main(String[] args) {
+     
+    int arr[]={1,2,2,3,4,4,5};
+     HashSet<Integer> hs = new HashSet<>();
+    for(int i=0;i<arr.length-1;i++){
+
+      if(arr[i]==arr[i+1])
+      {
+
+       hs.add(arr[i]);
+
+
+      }
+    }
+     System.out.println(hs);
+    }
+}
