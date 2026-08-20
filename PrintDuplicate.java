6af2c3bf10d705101db026162f8de5b22846e138
Sagar Patil <sagardpatil03@gmail.com>
2026-08-20 17:04:09 +0530

    Update project

--- PrintDuplicate.java
+++ PrintDuplicate.java
@@ -4,7 +4,7 @@ public class PrintDuplicate{
     public static void main(String[] args) {
      
     int arr[]={1,2,2,3,4,4,5};
-     HashSet<Integer> hs = new HashSet<>();
+     ArrayList<Integer> hs = new ArrayList<>();
     for(int i=0;i<arr.length-1;i++){
 
       if(arr[i]==arr[i+1])
