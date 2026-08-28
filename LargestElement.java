39529ac812e9eb4699cf09a8b482ef45884fbb56
Sagar Patil <sagardpatil03@gmail.com>
2026-08-28 16:15:31 +0530

    Add Java basics

--- LargestElement.java
+++ LargestElement.java
@@ -5,7 +5,7 @@
 
         int largest = arr[0];
 
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 1; i < arr.length; i++) {
             if(arr[i] > largest) {
                 largest = arr[i];
             }
