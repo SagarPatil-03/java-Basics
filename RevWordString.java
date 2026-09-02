9ff3ed7960b76689417a9362a64e307e4ef5a41c
Sagar Patil <sagardpatil03@gmail.com>
2026-09-02 10:22:47 +0530

    Update project

--- RevWordString.java
+++ RevWordString.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 class RevWordString {
+
+    // This is program used to spit words and trim remove space starts and ends.
     public static void main(String args[]) {
 
         String s = "sagar Devidas Patil";
