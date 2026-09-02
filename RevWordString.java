48dbc8e30b33d619f9a763d1540d9df1d20291a6
Sagar Patil <sagardpatil03@gmail.com>
2026-09-02 10:21:24 +0530

    Update project

---
+++ RevWordString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class RevWordString {
+    public static void main(String args[]) {
+
+        String s = "sagar Devidas Patil";
+
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            result.append(words[i]);
+
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        System.out.println(result.toString());
+    }
+}
+
+
+ 
