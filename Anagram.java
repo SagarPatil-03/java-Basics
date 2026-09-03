2aa50f3c7ac06e708557624495b3d72902228b3e
Sagar Patil <sagardpatil03@gmail.com>
2026-09-03 17:25:02 +0530

    Update project

--- Anagram.java
+++ Anagram.java
@@ -9,7 +9,7 @@ public class Anagram {
 
 
         s1=s1.replaceAll("\\s+","").toLowerCase();
-        s2=s2.replaceAll("\\s+","");
+        s2=s2.replaceAll("\\s+","").toLowerCase();
 
         char[] a = s1.toCharArray();
         char[] b = s2.toCharArray();
