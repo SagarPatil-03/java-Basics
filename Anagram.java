1a08a5ddab7ef9d51a2e234cfe69aa5368278598
Sagar Patil <sagardpatil03@gmail.com>
2026-09-10 10:48:08 +0530

    Update project

--- Anagram.java
+++ Anagram.java
@@ -9,7 +9,7 @@ public class Anagram {
 
 
         s1=s1.replaceAll("\\s+","").toLowerCase();
-        s2=s2.replaceAll("\\s+","").toLowerCase();
+        s2=s2.replaceAll("\\s+","").toLowerCase(); // toLowerCase() Or toUpperCase()
 
         char[] a = s1.toCharArray();
         char[] b = s2.toCharArray();
@@ -18,7 +18,7 @@ public class Anagram {
         Arrays.sort(b);
 
        // This is for anagram program .
-       
+
         if(Arrays.equals(a, b)) {
             System.out.println("Given String is Anagram String.");
         } else {
