9323e89d8891134fcab203ef9b81e576a344955a
Sagar Patil <sagardpatil03@gmail.com>
2026-09-25 19:21:08 +0530

    Update project

---
+++ AddSorted.java
@@ -0,0 +1,25 @@
+class AddSorted {
+
+    public static void main(String args[])
+    {
+      int[] arr1={1,2,3};
+      int[] arr2={4,5,6,7};
+
+      int[] result= new int(arr1.length + arr2.length);
+      int index=0;
+
+      for(int num :arr1)
+      {
+        result[index++]=num;
+      }
+
+      for(int num : arr2)
+      {
+        result[index++]=num;
+      }
+
+      System.out.println(Arrays.toString(result));
+
+    }
+    
+}
